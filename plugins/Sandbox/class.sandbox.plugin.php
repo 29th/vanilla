ae80a38a3fdd0a36f2e9fed4c90b65929b1512a3
wilson29thid <wilson@29th.org>
2015-01-02 23:03:41 -0500

    Added MarkdownBBCode, renamed 29th-rank to sandbox

--- plugins/Sandbox/class.sandbox.plugin.php
+++ plugins/Sandbox/class.sandbox.plugin.php
@@ -1,21 +1,8 @@
 <?php if (!defined('APPLICATION')) exit();
-/*	Copyright 2014 Zachary Doll
-*	This program is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
-*
-*	This program is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*	GNU General Public License for more details.
-*
-*	You should have received a copy of the GNU General Public License
-*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-$PluginInfo['x29thRank'] = array(
-	'Name' => '29th - Rank',
-	'Description' => 'Adds the rank to the author meta on discussions.',
+
+$PluginInfo['Sandbox'] = array(
+	'Name' => 'Sandbox',
+	'Description' => 'Plugin to play around in',
 	'Version' => '1.0',
 	//'RequiredApplications' => array('Yaga' => '0.5'),
 	'MobileFriendly' => TRUE,
@@ -25,7 +12,7 @@ $PluginInfo['x29thRank'] = array(
 	'License' => 'MIT'
 );
 
-class x29thRank extends Gdn_Plugin {
+class Sandbox extends Gdn_Plugin {
 
   public function DiscussionController_AuthorInfo_Handler($Sender) {
 /*    $Author = $Sender->EventArguments['Author'];
