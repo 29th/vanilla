44cb3c6dbcf9acd66d6a3a9f7ed219416b9b1fee
swomma <swomma@swomma.pl>
2016-03-27 21:50:44 +0000

    Removing "Image resized"

--- js/global.js
+++ js/global.js
@@ -1221,7 +1221,7 @@ jQuery(window).load(function() {
       var img = jQuery(img);
       var container = img.closest('div.Message');
       if (img.naturalWidth() > container.width() && container.width() > 0) {
-         img.after('<div class="ImageResized">' + gdn.definition('ImageResized', 'This image has been resized to fit in the page. Click to enlarge.') + '</div>');
+//         img.after('<div class="ImageResized">' + gdn.definition('ImageResized', 'This image has been resized to fit in the page. Click to enlarge.') + '</div>');
          img.wrap('<a href="'+$(img).attr('src')+'"></a>');
       }
    });
